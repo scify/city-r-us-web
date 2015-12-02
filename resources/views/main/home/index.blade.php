0f8cc7fdecd8aaa76cfbf5cc8483aa98362a7427
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2015-12-02 19:20:44 +0200

    CIT-239 finalize texts on first page

--- resources/views/main/home/index.blade.php
+++ resources/views/main/home/index.blade.php
@@ -72,27 +72,27 @@
               <div class="row features-list">
                   <div class="col-sm-4 wow fadeInLeft" data-wow-duration="1.5s" data-wow-offset="10" data-wow-delay="0.5s">
                       <div class="feature-icon">
-                          <i class="fa fa-laptop"></i>
+                          <i class="fa fa-flag"></i>
                       </div>
                       <h2>Βρες αποστολές</h2>
-                      <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies</p>
-                      <p><a class="btn btn-link" href="#" role="button">View details &raquo;</a></p>
+                      <p>Ο Δήμος τις Αθήνας προτείνει αποστολές για να βελτιώσουμε την πόλη.</p>
+
                   </div>
                   <div class="col-sm-4 wow fadeInLeft" data-wow-duration="1.5s" data-wow-offset="10" data-wow-delay="0.7s">
                       <div class="feature-icon">
-                          <i class="fa fa-lightbulb-o"></i>
+                          <i class="fa fa-map-marker"></i>
                       </div>
                       <h2>Ανέβασε διαδρομή</h2>
-                      <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies</p>
-                      <p><a class="btn btn-link" href="#" role="button">View details &raquo;</a></p>
+                      <p>Μπορείς να συνεισφέρεις σ'ενα δημόσιο χάρτη, στέλνοντας είτε ενα σημείο ή μια διαδρομή (ανάλογα με την αποστολή)</p>
+
                   </div>
                   <div class="col-sm-4 wow fadeInLeft" data-wow-duration="1.5s" data-wow-offset="10" data-wow-delay="0.9s">
                       <div class="feature-icon">
-                          <i class="fa fa-support"></i>
+                          <i class="fa fa-globe"></i>
                       </div>
                       <h2>Δες τον χάρτη της πόλης</h2>
-                      <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies</p>
-                      <p><a class="btn btn-link" href="#" role="button">View details &raquo;</a></p>
+                      <p>Η συνεισφορά όλων των πολίτων φαίνεται σένα δημόσιο χάρτη. Κλικ <a href="/map">εδώ </a></p>
+
                   </div>
               </div>
           </div>
@@ -100,58 +100,19 @@
               <div class="container">
                   <div class="row">
                       <div class="col-sm-4 wow fadeInLeft" data-wow-delay="0.5s" data-wow-duration="1.5s" data-wow-offset="10">
-                          <img src="{{ asset('img/home/iphone.png') }}" class="iphone-img" alt="">
+                          <img src="{{ asset('img/home/cityrus-mobileapp.png') }}" class="iphone-img" alt="">
                       </div>
                       <div class="col-sm-8 wow fadeInRight" data-wow-delay="0.5s" data-wow-duration="1.5s" data-wow-offset="10">
                           <h1>Λίγα λογια για την εφαρμογή</h1>
-                          <p>Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Nullam sagittis. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce id purus. Ut varius tincidunt libero.</p>
+                          <p>Το City-R-US ειναι μια εφαρμογή που επιτρέπει στους κατοίκους της Αθήνας να συμμετέχουν σε αποστολές.
+                             Επίλεξε την αποστολή που σε ενδιαφέρει και βοήθησε τη πόλη σου!
+                             Τα δεδομένα των αποστολών συλλέγονται σ'έναν δημόσιο χάρτη διαθέσιμο <a href="/map">εδώ</a></p>
                           <ul class="list-unstyled features-list-2">
-                              <li><i class="fa fa-diamond icon-state-success m-r-xs icon-md"></i>Unique design</li>
-                              <li><i class="fa fa-check icon-state-success m-r-xs icon-md"></i>Everything you need</li>
-                              <li><i class="fa fa-cogs icon-state-success m-r-xs icon-md"></i>Tons of features</li>
-                              <li><i class="fa fa-cloud icon-state-success m-r-xs icon-md"></i>Easy to use &amp; customize</li>
+                              <li><i class="fa fa-diamond icon-state-success m-r-xs icon-md"></i>Μοντέρνα αισθητική</li>
+                              <li><i class="fa fa-check icon-state-success m-r-xs icon-md"></i>Μπορείς να προτείνεις κ εσύ αποστολές!</li>
+                              <li><i class="fa fa-cogs icon-state-success m-r-xs icon-md"></i>Επιβράβευση των πολιτών που συνεισφέρουν περισσότερο</li>
+                              <li><i class="fa fa-cloud icon-state-success m-r-xs icon-md"></i>Εύκολο στη χρήση του</li>
                           </ul>
-                      </div>
-                  </div>
-              </div>
-          </section>
-          <section id="section-2">
-              <div class="container">
-                  <div class="row">
-                      <div class="col-sm-8 wow fadeInLeft" data-wow-delay="0.5s" data-wow-duration="1.5s" data-wow-offset="10">
-                          <section>
-                              <div class="tabs tabs-style-linebox">
-                                  <nav>
-                                      <ul>
-                                          <li class="tab-current"><a href=""><span>Responsive</span></a></li>
-                                          <li class=""><a href=""><span>Browsers</span></a></li>
-                                          <li class=""><a href=""><span>Bootstrap</span></a></li>
-                                          <li class=""><a href=""><span>Icons</span></a></li>
-                                          <li class=""><a href=""><span>Documentation</span></a></li>
-                                      </ul>
-                                  </nav>
-                                  <div class="content-wrap">
-                                      <section class="content-current">
-                                          <h1>Πρότεινε μια αποστολή</h1>
-                                          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.<br>Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.</p></section>
-                                      <section><p>
-                                          <h1>Cross-browser Compatible</h1>
-                                          <p>Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.<br>Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna.</p></section>
-                                      <section><p>
-                                          <h1>Built With Bootstrap 3.3.4</h1>
-                                          <p>Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Maecenas malesuada. Praesent congue erat at massa. Sed cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis porttitor congue, elit erat euismod orci, ac placerat dolor lectus quis orci. Phasellus consectetuer vestibulum elit. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc.</p></section>
-                                      <section><p>
-                                          <h1>+1100 Icons Included</h1>
-                                          <p>Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Nullam sagittis.<br>Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce id purus. Ut varius tincidunt libero. Phasellus dolor. Maecenas vestibulum mollis diam.</p></section>
-                                      <section><p>
-                                          <h1>Well Documented</h1>
-                                          <p>Morbi nec metus. Phasellus blandit leo ut odio. Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Sed magna purus, fermentum eu, tincidunt eu, varius ut, felis. In auctor lobortis lacus. Quisque libero metus, condimentum nec, tempor a, commodo mollis, magna. Vestibulum ullamcorper mauris at ligula. Fusce fermentum. Nullam cursus lacinia erat. Praesent blandit laoreet nibh.</p></section>
-                                  </div>
-                              </div>
-                          </section>
-                      </div>
-                      <div class="col-sm-4 wow fadeInRight" data-wow-delay="0.5s" data-wow-duration="1.5s" data-wow-offset="10">
-                          <img src="assets/images/iphone2.png" class="iphone-img" alt="">
                       </div>
                   </div>
               </div>
@@ -208,7 +169,7 @@
                               <div class="form-group">
                                   <div class="row">
                                       <div class="col-sm-6">
-                                          <input type="text" class="form-control input-lg contact-name" placeholder="Name">
+                                          <input type="text" class="form-control input-lg contact-name" placeholder="Όνομα">
                                       </div>
                                       <div class="col-sm-6">
                                           <input type="email" class="form-control input-lg" placeholder="Email">
@@ -216,12 +177,9 @@
                                   </div>
                               </div>
                               <div class="form-group">
-                                  <input type="text" class="form-control input-lg" placeholder="Subject">
+                                  <textarea class="form-control" rows="4=6" placeholder="Θα σας πρότεινα να..."></textarea>
                               </div>
-                              <div class="form-group">
-                                  <textarea class="form-control" rows="4=6" placeholder="Message"></textarea>
-                              </div>
-                              <button type="submit" class="btn btn-default btn-lg">Send Message</button>
+                              <button type="submit" class="btn btn-default btn-lg">Aποστολή</button>
                           </form>
                       </div>
                   </div>
@@ -229,7 +187,7 @@
           </div>
           <footer>
               <div class="container">
-                  <p class="text-center no-s">2015 &copy; SciFY</p>
+                  <p class="text-center no-s">2015 &copy; SciFY | <a href="https://commons.wikimedia.org/wiki/File:Athens_-_Monastiraki_square_and_station_-_20060508.jpg" target="_blank">Πηγή εικόνας background</a></p>
               </div>
           </footer>
 
