c27fa62873121e4307deb125a43dd8d9732f3902
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2015-12-02 16:35:52 +0200

    CIT-236

--- resources/views/main/home/index.blade.php
+++ resources/views/main/home/index.blade.php
@@ -203,7 +203,7 @@
                   <div class="row">
                       <div class="col-sm-6 col-sm-offset-3 wow rotateInUpLeft" data-wow-duration="1.5s" data-wow-offset="10" data-wow-delay="0.5s">
                           <a href="#contact" class="btn btn-success btn-lg btn-rounded contact-button"><i class="fa fa-envelope-o"></i></a>
-                          <h2>Let's keep in touch</h2>
+                          <h2>Πρότεινε μια αποστολή</h2>
                           <form class="m-t-md">
                               <div class="form-group">
                                   <div class="row">
