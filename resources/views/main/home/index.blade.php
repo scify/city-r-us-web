f4ed7e3977aa8c589d133e0a280a53994a573197
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2015-12-16 18:59:27 +0200

    CIT-280

--- resources/views/main/home/index.blade.php
+++ resources/views/main/home/index.blade.php
@@ -91,7 +91,7 @@
                           <i class="fa fa-globe"></i>
                       </div>
                       <h2>Δες τον χάρτη της πόλης</h2>
-                      <p>Η συνεισφορά όλων των πολίτων φαίνεται σένα δημόσιο χάρτη. Κλικ <a href="/map">εδώ </a></p>
+                      <p>Η συνεισφορά όλων των πολιτών φαίνεται σένα δημόσιο χάρτη. Κλικ <a href="/map">εδώ </a></p>
 
                   </div>
               </div>
@@ -125,34 +125,35 @@
                           <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
                               <ol class="carousel-indicators">
                                   <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                                  <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                                  <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                                  {{--<li data-target="#carousel-example-generic" data-slide-to="1"></li>--}}
+                                  {{--<li data-target="#carousel-example-generic" data-slide-to="2"></li>--}}
                               </ol>
                               <div class="carousel-inner" role="listbox">
                                   <div class="item active">
                                       <div class="row">
                                           <div class="col-sm-6 col-sm-offset-3">
-                                              <p class="text-white">â€œAenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Nullam sagittis. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce id purus. Ut varius tincidunt libero.â€</p>
-                                              <span>- David, App Manager</span>
+                                              <p class="text-white">Πολύ καλή ιδέα!<br> Θα είναι εξαιρετικό να έχουμε χαρτογραφημένα σημεία και διαδρομές που είναι προσβάσιμες από ΑμεΑ στην πόλη.<br> Πείτε μου πως να βοηθήσω.</p>
+                                              <span>Dr. Γιώργος Χρηστάκης
+                                                     <br>Χορογράφος, <br>χορευτής σε αναπ.αμαξίδιο, <br>ιδρυτής του Χοροθεάτρου ΔΑΓΙΠΟΛΗ</span>
                                           </div>
                                       </div>
                                   </div>
-                                  <div class="item">
-                                      <div class="row">
-                                          <div class="col-sm-6 col-sm-offset-3">
-                                              <p class="text-white">â€œLorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.â€</p>
-                                              <span>- Sandra, Director</span>
-                                          </div>
-                                      </div>
-                                  </div>
-                                  <div class="item">
-                                      <div class="row">
-                                          <div class="col-sm-6 col-sm-offset-3">
-                                              <p>â€œCurabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Maecenas malesuada. Praesent congue erat at massa. Sed cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis porttitor congue, elit erat euismod orci, ac placerat dolor lectus quis orci. Phasellus consectetuer vestibulum elit.â€</p>
-                                              <span>- Amily, UI Designer</span>
-                                          </div>
-                                      </div>
-                                  </div>
+                                  {{--<div class="item">--}}
+                                      {{--<div class="row">--}}
+                                          {{--<div class="col-sm-6 col-sm-offset-3">--}}
+                                              {{--<p class="text-white">â€œLorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.â€</p>--}}
+                                              {{--<span>- Sandra, Director</span>--}}
+                                          {{--</div>--}}
+                                      {{--</div>--}}
+                                  {{--</div>--}}
+                                  {{--<div class="item">--}}
+                                      {{--<div class="row">--}}
+                                          {{--<div class="col-sm-6 col-sm-offset-3">--}}
+                                              {{--<p>â€œCurabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Maecenas malesuada. Praesent congue erat at massa. Sed cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis porttitor congue, elit erat euismod orci, ac placerat dolor lectus quis orci. Phasellus consectetuer vestibulum elit.â€</p>--}}
+                                              {{--<span>- Amily, UI Designer</span>--}}
+                                          {{--</div>--}}
+                                      {{--</div>--}}
+                                  {{--</div>--}}
                               </div>
                           </div>
                       </div>
