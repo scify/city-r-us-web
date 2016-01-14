1c3e8ccf436f0279a698fd5f933548183707fa38
Nick Zorbas <nickzorb@gmail.com>
2016-01-14 17:24:57 +0200

    [CIT-306] Added datepick in filters section

--- resources/views/main/home/map.blade.php
+++ resources/views/main/home/map.blade.php
@@ -10,12 +10,15 @@
 @stop
 
 @section('footerScripts')
+    <link href="{{ asset('/plugins/bootstrap-datepicker/css/datepicker3.css')}}" rel="stylesheet" type="text/css"/>
+    <script src="{{ asset('/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js')}}"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDEahTQQgG0h61XN8n5LNS4NTx5NsbWaXg&callback="></script>
     <script src="{{ asset('/plugins/googleMapsOverlappingMarker/oms.min.js')}}"></script>
     <script src="{{ asset('/plugins/googleMapsMarkerClusterer/markerclusterer.js')}}"></script>
     <script src="{{ asset('/js/pages/home/activityonmap.js')}}"></script>
     <script>
         $(function () {
+            $('.datepicker').datepicker({autoclose: true, format: 'dd/mm/yyyy'});
 
             var mapActivity = new scify.ActivityOnMap($("#map-container"),
                     $("#map-container").data("marker-icon"),
@@ -53,17 +56,38 @@
             <div id="filters">
                 <h1>Eπιλογή αποστολής</h1>
                 @if ($missions->status=="success")
-                <div id="missions">
-                    @foreach($missions->message->missions as $m)
-                    <div data-id="{{ $m->id }}" data-type="{{ $m->type_id }}" class="mission" title="{{ $m->description }}">{{ $m->name }}</div>
-                    @endforeach
-                </div>
+                    <div id="missions">
+                        @foreach($missions->message->missions as $m)
+                        <div data-id="{{ $m->id }}" data-type="{{ $m->type_id }}" class="mission" title="{{ $m->description }}">{{ $m->name }}</div>
+                        @endforeach
+                    </div>
+                    <h1>Eπιλογή Περιόδου</h1>
+                    <div id="period">
+                        <div class="datepick-wrapper">
+                            Από:
+                            <div class="input-group date datepicker">
+                                <input type="text" class="form-control">
+                                <span class="input-group-addon">
+                                    <i class="glyphicon glyphicon-th"></i>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="datepick-wrapper">
+                            Έως:
+                            <div class="input-group date datepicker">
+                                <input type="text" class="form-control">
+                                <span class="input-group-addon">
+                                    <i class="glyphicon glyphicon-th"></i>
+                                </span>
+                            </div>
+                        </div>
+                        <input type="button" class="btn btn-danger" value="Ανανέωση">
+                    </div>
                 @else
-                <div>
-                    Συνέβει ένα λάθος κατά την φόρτωση αποστολών...
-                </div>
+                    <div>
+                        Συνέβει ένα λάθος κατά την φόρτωση αποστολών...
+                    </div>
                 @endif
-
             </div>
         </div>
         <div data-marker-icon=""
